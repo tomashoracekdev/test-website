c4b305ddb00ffd399e25a39132464d12b449eaf4
tomashoracekdev <118591548+tomashoracekdev@users.noreply.github.com>
2024-01-28 21:06:07 +1100

    Update email_append.php

--- email_append.php
+++ email_append.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="descripion 1">
-        <title>Hello world</title>
+        <?php echo $_GET["email"]; ?>
+        <br>
+        <?php echo $_GET["name"]; ?>
     </head>
 </html>
